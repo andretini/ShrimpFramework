5584c6632f3a213a69db0e4be10e1d8051a5129d
andretini <andrealijaramosagostini@gmail.com>
2025-08-26 20:42:53 -0300

    config package

--- ShrimpFramework/obj/Debug/net9.0/ShrimpFramework.AssemblyInfo.cs
+++ ShrimpFramework/obj/Debug/net9.0/ShrimpFramework.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShrimpFramework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+974bb3764231cdc102303f9dbd902ae114ff5697")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShrimpFramework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShrimpFramework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
